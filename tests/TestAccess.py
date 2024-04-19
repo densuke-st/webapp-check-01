614e63363bd470a1b0d41c93e783532c9c74dd22
SATO Daisuke <densuke@st.kobedenshi.ac.jp>
2024-04-19 01:31:39 +0000

    問題挿入

--- tests/TestAccess.py
+++ tests/TestAccess.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.by import By
 import time
 import unittest
 import sys
 
+
 # リモートサーバーのアドレス
 REMOTE_URL = 'http://selenium:4444/wd/hub'
+
+CHECK_WORD = "Hello,PHP"
 
 class SampleTest(unittest.TestCase):
     def setUp(self):
@@ -15,20 +19,21 @@ class SampleTest(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    def test_access(self):
-        self.driver.get('http://web/')
-        time.sleep(2)
-        self.driver.save_screenshot('screenshot.png')
+    def test_Hello_using_PHP(self):
+         """./public/hello.phpを読み込み、
+         PHPのコードを示す'<?php'が含まれていることを確認する"""
+         with open('./public/hello.php') as f:
+             php_code = f.read()
+             self.assertIn('<?php', php_code)
 
-    def test_title_fail(self):
-        self.driver.get('http://web/')
+    def test_Hello(self):
+        """http://web/hello.php へアクセスし、
+        最初のpタグにCHECK_WORD変数と同じ文字列があることをテストする
+        """
+        self.driver.get('http://web/hello.php')
         time.sleep(2)
-        self.assertTrue('PhP' in self.driver.title)
-
-    def test_title_PHP(self):
-        self.driver.get('http://web/')
-        time.sleep(2)
-        self.assertTrue('PHP' in self.driver.title)
+        p = self.driver.find_element(by=By.TAG_NAME, value='p')
+        self.assertEqual(p.text, CHECK_WORD)
 
 if __name__ == '__main__':
     unittest.main()
